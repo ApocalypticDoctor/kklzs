12710ea0adb97f45cba31b3ddb70d0de46e0c2b2
可比克 <3343940029@qq.com>
2025-06-18 17:48:28 +0800

    添加更新进度条

--- background/update.py
+++ background/update.py
@@ -21,34 +21,31 @@ except:
         version_new = version_now
 
 def update(progress_callback):
-    progress_callback(20)
-    time.sleep(1)
-    progress_callback(100)
-    # response = requests.get(repo_url, timeout=1)
-    # total_size = len(response.content) - 26390760 - 187798
-    # downloaded_size = 0
-    # last = 0
-    # with (zipfile.ZipFile(io.BytesIO(response.content)) as zip_file):
-    #     for file_info in zip_file.infolist():
-    #         if (file_info.filename.startswith('kklzs-master/background/') or \
-    #             file_info.filename.startswith('kklzs-master/template/')) and \
-    #             not file_info.filename.endswith('.exe') and not file_info.filename.endswith('.ttf'):
-    #
-    #             target_path = os.path.join('../temp', os.path.relpath(file_info.filename, 'kklzs-master'))
-    #
-    #             if file_info.is_dir():
-    #                 os.makedirs(target_path, exist_ok=True)
-    #             else:
-    #                 os.makedirs(os.path.dirname(target_path), exist_ok=True)
-    #                 with zip_file.open(file_info) as src, open(target_path, 'wb') as dst:
-    #                     chunk_size = 8192
-    #                     while True:
-    #                         chunk = src.read(chunk_size)
-    #                         if not chunk:
-    #                             break
-    #                         dst.write(chunk)
-    #                         downloaded_size += len(chunk)
-    #                         percent = int((downloaded_size / total_size) * 100)
-    #                         if percent and last != percent:
-    #                             last = percent
-    #                             progress_callback(percent)
+    response = requests.get(repo_url, timeout=1)
+    total_size = len(response.content) - 26390760 - 187798
+    downloaded_size = 0
+    last = 0
+    with (zipfile.ZipFile(io.BytesIO(response.content)) as zip_file):
+        for file_info in zip_file.infolist():
+            if (file_info.filename.startswith('kklzs-master/background/') or \
+                file_info.filename.startswith('kklzs-master/template/')) and \
+                not file_info.filename.endswith('.exe') and not file_info.filename.endswith('.ttf'):
+
+                target_path = os.path.join('../temp', os.path.relpath(file_info.filename, 'kklzs-master'))
+
+                if file_info.is_dir():
+                    os.makedirs(target_path, exist_ok=True)
+                else:
+                    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+                    with zip_file.open(file_info) as src, open(target_path, 'wb') as dst:
+                        chunk_size = 8192
+                        while True:
+                            chunk = src.read(chunk_size)
+                            if not chunk:
+                                break
+                            dst.write(chunk)
+                            downloaded_size += len(chunk)
+                            percent = int((downloaded_size / total_size) * 100)
+                            if percent and last != percent:
+                                last = percent
+                                progress_callback(percent)
